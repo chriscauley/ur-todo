c9688b79665f2b99f11c2bd1ea69e483e31e9574
chriscauley <chris@lablackey.com>
2019-01-24 02:52:26 +0000

    serving index.html out of /

--- main/urls.py
+++ main/urls.py
@@ -1,15 +1,20 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, re_path, include
+from django.views.static import serve
 
 import unrest.views
 import main.views
 from unrest.nopass.views import create as nopass_create
 
 urlpatterns = [
-    path("", unrest.views.redirect,kwargs={'url':'/static/index.html'}),
     path('admin/', admin.site.urls),
     path('api/nopass/',include('unrest.nopass.urls')),
     path("user.json",unrest.views.user_json),
     path("api/auth/register/",nopass_create),
     re_path('^api/(main).(Task|Project|Activity)/$', main.views.obj_api),
 ]
+
+if settings.DEBUG:
+    kwargs = { 'path': 'index.html', 'document_root': settings.DIST_DIR }
+    urlpatterns.append(path('', serve, kwargs=kwargs))
